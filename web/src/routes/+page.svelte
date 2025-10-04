f7fe14ca3af598a3dc485403c9db66cc474271be
Carson Webster <carsonwebster@hotmail.com>
2025-10-04 16:23:17 -0700

    Added missing nasa logo

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import ChatContainer from '$lib/components/ChatContainer.svelte';
+	import nasaicon from '$lib/assets/nasa-logo.svg';
 </script>
 
 <main class="flex h-screen flex-col">
 	<!-- Top navigation / branding -->
 	<header class="flex items-center justify-between border-b bg-white px-6 py-3 shadow-sm">
 		<div class="flex items-center gap-2">
-			<img src="/nasa-logo.svg" alt="NASA" class="text-md fond-bold h-6 text-black" />
+			<img src={nasaicon} alt="NASA" class="text-md fond-bold h-6 text-black" />
 			<span class="text-sm font-semibold text-gray-700">Bio Studies AI Dashboard</span>
 		</div>
 		<nav class="text-sm text-gray-500">
